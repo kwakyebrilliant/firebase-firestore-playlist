ec5e1e7081ad816fba447e191ec2eefe95bdd2c1
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-10-06 12:54:58 +0000

    app updates

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+//refrence
+const cafeList = document.querySelector('#cafe-list');
+
+// create element & render cafe
+
+
+//getting collections
 db.collection('cafes').get().then(snapshot => {
     snapshot.docs.forEach(doc => {
         console.log(doc.data());
