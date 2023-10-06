23eb11be0ab0f024a2bfdae601826d042e1d19ef
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-10-06 13:13:31 +0000

    app updates

--- app.js
+++ app.js
@@ -2,11 +2,18 @@
 const cafeList = document.querySelector('#cafe-list');
 
 // create element & render cafe
+function renderCafe(doc){
 
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
+
+
+}
 
 //getting collections
 db.collection('cafes').get().then(snapshot => {
     snapshot.docs.forEach(doc => {
-        console.log(doc.data());
+        renderCafe(doc);
     });
 });
