d20ac619828f18a5f06521563c92c59ec4d41e29
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-10-07 01:05:02 +0000

    app updates

--- app.js
+++ app.js
@@ -31,11 +31,11 @@ function renderCafe(doc){
 }
 
 //getting collections
-db.collection('cafes').orderBy('city').get().then(snapshot => {
-    snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-    });
-});
+// db.collection('cafes').orderBy('city').get().then(snapshot => {
+//     snapshot.docs.forEach(doc => {
+//         renderCafe(doc);
+//     });
+// });
 
 //saving data
 form.addEventListener('submit', (e) => {
@@ -46,4 +46,18 @@ form.addEventListener('submit', (e) => {
     });
     form.name.value = '';
     form.city.value = '';
+});
+
+// real-time listener
+db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        // console.log(change.doc.data());
+        if(change.type == 'added'){
+            renderCafe(change.doc);
+        } else if (change.type == 'removed'){
+            let li = cafeList.querySelector('[data-id=' + change.doc.id + ']');
+            cafeList.removeChild(li);
+        }
+    });
 });
